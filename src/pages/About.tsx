b7b5b88ee4a28a2229b1e472bbe8e37a0a2e0035
James <132644965+jimothy-dev@users.noreply.github.com>
2026-09-15 15:14:25 -0700

    About: drop the notebook link

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -29,7 +29,6 @@ export default function About() {
       <div className="card">
         <ul>
           <li><a href="https://github.com/jimothy-dev/CTD_Grapher_Web" {...ext}>Source on GitHub</a></li>
-          <li><a href="https://colab.research.google.com/github/jimothy-dev/CTD_Grapher_v2/blob/main/CTD_Grapher_v2.ipynb" {...ext}>The Colab notebook</a> it grew out of</li>
           <li><a href="https://doi.org/10.5281/zenodo.22371639" {...ext}>Cite it</a>: Simpson, J. (2026). CTD Grapher. Zenodo, doi 10.5281/zenodo.22371639</li>
           <li><a href="https://github.com/jimothy-dev" {...ext}>jimothy-dev</a></li>
         </ul>
